43f51ec96c720638f15d986d79b4efa4f408eeef
Abraham Rivera-Garcia <arivera178@west-mec.org>
2020-02-03 21:25:41 -0700

    Completed Case 1 Push/Commit

---
+++ case1/tc_order.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+  
+   Author: Abraham Rivera-Garcia
+   Date: February 3rd, 2020
+     
+   Filename: tc_order.js  
+
+   The item array contains the ID numbers of the items ordered by the customer
+   The itemDescription array contains the description of each item
+   The itemPrice array contains the price of each item
+   The itemQty array contains the quantity ordered of each item
+   
+*/
+//Declares variable and all the items in the array
+var item = [10582, 23015, 41807, 10041];
+var itemDescription = ["1975 Green Bay Packers Football(signed), item 10582", "Tom landry 1955 Football Card(unsigned), item 23015", 
+"1916 Army-Navy Game, Framed photo (signed), item 41807", "Protective Card Sheets, item 10041"];
+var itemPrice = [149.93, 89.98, 334.93, 22.67];
+var itemQty = [1, 1, 1, 4];
+
